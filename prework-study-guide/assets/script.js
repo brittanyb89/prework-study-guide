2045def176472e4cfa579c69ffcf0dac73dc975f
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-10-14 12:13:34 -0500

    commented out conditional statement

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -4,6 +4,7 @@ for(var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
 }
 
+/*
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -15,4 +16,4 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
-
+*/
