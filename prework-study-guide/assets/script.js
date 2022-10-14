4988793c359fb277bcf786800a8fa1b53eb0cffc
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-10-14 17:43:02 -0500

    created new function named selectTopics

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -6,16 +6,18 @@ function listTopics() {
     }
 }
 
-/*
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
+function selectTopics() {
+    if (topic === 'HTML') {
+        console.log("Let's study HTML!");
+      } else if (topic === 'CSS') {
+        console.log("Let's study CSS!");
+      } else if (topic === 'Git') {
+        console.log("Let's study Git!");
+      } else if (topic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+      } else {
+        console.log('Please try again!');
+      }
 }
-*/
+
+
